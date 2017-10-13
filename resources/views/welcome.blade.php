0072ac983c010822de2af9ed8ac5d512b2458e24
SawupKhan <sawrupkhan@yahoo.com>
2017-10-14 00:14:23 +0600

    fafdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rajshahi Medical College
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/t', function () {
+    return view('doctor');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
